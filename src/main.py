6bd9fcc2415f8a7eafa9237d6eaf1444f948a03b
tauesp127-alexis <gestionti@taurus.com.mx>
2025-02-13 11:00:20 -0600

    Formatear tabla de DB

--- src/main.py
+++ src/main.py
@@ -1,18 +1,20 @@
-from conn_sqlserver import t_areas,t_Det_Funcion
+from conn_sqlserver import t_areas,t_Det_Funcion, t_dig_area_auto,t_dig_areat,t_dig_tc, t_dig_tipoPuesto
 
-areas = []
-puestos = []
-
-
-def asignar_valores(table, *name_col):
+### Formatear Datos en DB
+def formatear_tabla(table, **cols):
     new_table = []
     for row in table:
-        new_table.append(dict(id_area=row[0], nombre_funcion=row[1]))
-        
+        pre_row = dict()
+        for key, value in cols.items():
+            pre_row[key] = row[value]
 
-for area in t_areas:
-    pass
-
-for puesto in t_Det_Funcion:
-    puestos.append(dict(id_area=puesto[0], nombre_funcion=puesto[1]))
+        new_table.append(pre_row)
+    return new_table    
     
+areas = formatear_tabla(t_areas, id_area=0, nombre_area=1)
+puestos = formatear_tabla(t_Det_Funcion, id_funcion=0, nombre_puesto=1)
+responsables_area = formatear_tabla(t_dig_area_auto, id_res_area=0, area=1, nombre_res=2)
+areas_internas = formatear_tabla(t_dig_areat, id_areat=0, nombre_areat=1)
+tipo_puesto = formatear_tabla(t_dig_tipoPuesto, id_tipoPuesto=0, nombre=1)
+nivel_puesto = formatear_tabla(t_dig_tc, id_nivelPuesto=0, nombre=1)
+
